14579ec6785f123e1b8c7ce99eb4fe0832935990
Ashish-Dobhal-Devops <154491535+Ashish-Dobhal-Devops@users.noreply.github.com>
2024-08-15 13:51:34 +0530

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -1,29 +1,63 @@
 # Security Groups for Hub VPC
-module "hub_security_group" {
-  source  = "terraform-aws-modules/security-group/aws//modules/http-80-443"
-  version = "4.5.0"
-
+resource "aws_security_group" "hub_security_group" {
   name   = "hub-security-group"
   vpc_id = module.hub_vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  egress_cidr_blocks  = var.prod_private_subnets
-  egress_rules        = ["http-80-tcp", "https-443-tcp"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Hub-Security-Group"
+  }
 }
 
 # Security Groups for Prod VPC allowing ingress from Hub VPC's public LB subnet
-module "prod_security_group" {
-  source  = "terraform-aws-modules/security-group/aws//modules/http-80-443"
-  version = "4.5.0"
-
+resource "aws_security_group" "prod_security_group" {
   name   = "prod-security-group"
   vpc_id = module.prod_vpc.vpc_id
 
-  ingress_cidr_blocks = [module.hub_vpc.public_subnets[0]] # Allow ingress from Hub-prod-public-LB-SN
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-  egress_rules        = ["http-80-tcp", "https-443-tcp"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [element(module.hub_vpc.public_subnets, 0)]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [element(module.hub_vpc.public_subnets, 0)]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Prod-Security-Group"
+  }
 }
 
 # Additional Security Group for SSH allowing ingress from Hub-admin-public-SN
@@ -39,14 +73,14 @@ resource "aws_security_group" "hub_admin_sg" {
   }
 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.prod_private_subnets
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Hub-admin-public-SG"
+    Name = "Hub-Admin-SG"
   }
 }
 
@@ -65,31 +99,24 @@ resource "aws_security_group" "prod_private_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [module.hub_vpc.public_subnets[0]]
+    cidr_blocks = [element(module.hub_vpc.public_subnets, 0)]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [module.hub_vpc.public_subnets[0]]
+    cidr_blocks = [element(module.hub_vpc.public_subnets, 0)]
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Prod-private-SG"
+    Name = "Prod-Private-SG"
   }
 }
