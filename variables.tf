56b39dd1ddcfffe2cf792527a35ec5d5ff66a8eb
Ashish-Dobhal-Devops <154491535+Ashish-Dobhal-Devops@users.noreply.github.com>
2024-08-15 13:27:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,3 +45,9 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+  default     = "my_terraform_key"
+}
